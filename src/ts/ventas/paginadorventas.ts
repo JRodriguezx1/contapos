29d6ffee794863c6fc9c1d1baba7e3e3f963a7fe
Julian Rodriguez <julianlink123@hotmail.com>
2025-08-01 13:00:47 -0500

    filtro de categorias en ventas

--- src/ts/ventas/paginadorventas.ts
+++ src/ts/ventas/paginadorventas.ts
@@ -1,13 +1,24 @@
 (()=>{
     if(document.querySelector('.paginadorventas')){
 
+      const filtrocategorias = document.querySelectorAll('.filtrocategorias');
+
       var options = {
-        valueNames: [ 'card-title', 'card-category'],
-        page: 15,
+        valueNames: [ 'card-producto', { data: ['categoria'] }],
+        page: 18,
         pagination: true,
       };
 
       var hackerList = new List('hacker-list', options);
-      }
+
+
+      filtrocategorias.forEach(element => {
+        element.addEventListener('click', (e)=>{
+          const categoria:string = (e.target as HTMLElement).dataset.categoria!;
+          hackerList.filter();
+        });
+      });
+
+    }
 
 })();
