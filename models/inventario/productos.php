60308a1b202ec3061e865feefc2f71fdb6ae6aca
Julian Rodriguez <julianlink123@hotmail.com>
2025-10-06 19:16:32 -0500

    ajustando el carrito de solicitar inventario otra sede

--- models/inventario/productos.php
+++ models/inventario/productos.php
@@ -42,20 +42,15 @@ class productos extends \Model\ActiveRecord {
 
     // Validación para productos nuevos
     public function validar_nuevo_producto():array {
+        if(!$this->idcategoria || !is_numeric($this->idcategoria))self::$alertas['error'][] = 'La categoria del producto no seleccionada';
         if(!$this->nombre)self::$alertas['error'][] = 'El Nombre del producto es Obligatorio';
-        
         if(strlen($this->nombre)>51)self::$alertas['error'][] = 'El Nombre del producto no debe superar los 52 caracteres';
-        
         if($this->sku)
           if(strlen($this->sku)>15)self::$alertas['error'][] = 'El codigo del producto no debe ser mayor a 15 digitos';
-        
         if($this->descripcion)
           if(strlen($this->descripcion)>249)self::$alertas['error'][] = 'La descripcion no debe supear los 249 caracteres';
-        
         //if(!$this->stock)self::$alertas['error'][] = 'Colocar el stock inicial del producto';
-
         //if(!$this->precio_compra)self::$alertas['error'][] = 'Colocar el precio de compra del producto';
-
         if(!$this->precio_venta)self::$alertas['error'][] = 'Colocar el precio de venta del producto';
         return self::$alertas;
     }
