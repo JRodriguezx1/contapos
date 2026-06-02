4e89971a1ebc82e5e17bdd4fc508a99085a0e570
Julian Rodriguez <julianlink123@hotmail.com>
2026-06-01 19:58:19 -0500

    correcion de texto en modulo de ventas

--- views/admin/ventas/modalguardarpedido.php
+++ views/admin/ventas/modalguardarpedido.php
@@ -5,7 +5,7 @@
           <p class="text-xl text-gray-500">La orden No: <?php echo $num_orden;?> se registrara en sistema. Podra volver a retomarla.</p>
       </div>
       <div id="" class="flex justify-around w-full border-t border-gray-300 pt-6">
-          <button class="btn-md btn-blue cotizacion">Cotzacion</button>
+          <button class="btn-md btn-blue cotizacion">Cotizacion</button>
           <button class="btn-md btn-indigo remision">Remision</button>
       </div>
   </dialog>
